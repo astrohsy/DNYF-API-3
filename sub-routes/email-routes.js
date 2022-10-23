32fb2aa0db86611e6995a6b5260227dcbfb0a2a8
mchen132 <Tom Chen>
2022-10-23 00:57:14 -0400

    Add endpoints stub

--- sub-routes/email-routes.js
+++ sub-routes/email-routes.js
@@ -6,4 +6,24 @@ router.get("/", (req, res, next) => {
     res.status(200).json({uid: req.uid})
 });
 
+router.post("/", (req, res, next) => {
+    res.status(200).json({uid: req.uid})
+});
+
+router.get("/verify", (req, res, next) => {
+    res.status(200).json({uid: req.uid})
+});
+
+router.post("/verify", (req, res, next) => {
+    res.status(200).json({uid: req.uid})
+});
+
+router.get("/status", (req, res, next) => {
+    res.status(200).json({uid: req.uid})
+});
+
+router.post("/status", (req, res, next) => {
+    res.status(200).json({uid: req.uid})
+});
+
 module.exports = router;
