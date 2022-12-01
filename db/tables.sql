17696ed94e6f4627d081e87dc9269f93edbb550c
mchen132 <Tom Chen>
2022-12-01 01:35:41 -0500

    Update DB Schema

--- db/tables.sql
+++ db/tables.sql
@@ -1,24 +1,25 @@
 use `COMS6156-contacts-db`;
 
-CREATE TABLE User (
-	uid INT, 
-    PRIMARY KEY (uid)
-);
-
 CREATE TABLE Email (
-	email VARCHAR(128) NOT NULL,
+    email VARCHAR(128) NOT NULL,
     verified TINYINT NOT NULL DEFAULT 0, 
-    uid INT,
-    UNIQUE(email),
-    PRIMARY KEY (uid, email),
-    FOREIGN KEY (uid) REFERENCES User(uid) ON UPDATE CASCADE ON DELETE CASCADE
+    uid INT NOT NULL,
+    UNIQUE(uid),
+    PRIMARY KEY(email)
 );
 
 CREATE TABLE Phone (
-	phone_number VARCHAR(16) NOT NULL,
+    phone_number VARCHAR(16) NOT NULL,
     verified TINYINT NOT NULL DEFAULT 0, 
-    uid INT,
-    UNIQUE(phone_number),
-    PRIMARY KEY (uid, phone_number),
-    FOREIGN KEY (uid) REFERENCES User(uid) ON UPDATE CASCADE ON DELETE CASCADE
+    uid INT NOT NULL,
+    UNIQUE(uid),
+    PRIMARY KEY(phone_number)
+);
+
+CREATE TABLE ZipCode (
+    zip_code VARCHAR(5) NOT NULL,
+    verified TINYINT NOT NULL DEFAULT 0, 
+    uid INT NOT NULL,
+    UNIQUE(uid),
+    PRIMARY KEY(zip_code)
 );
