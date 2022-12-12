ae0a2c14e4d8ecbbc82126247e0cb36936614eed
mchen132 <Tom Chen>
2022-12-11 19:52:04 -0500

    Add zip code verification to combined record POST route

--- controllers/combined-record-controller.js
+++ controllers/combined-record-controller.js
@@ -1,6 +1,7 @@
 const emailQueries = require('../db/email-queries');
 const phoneQueries = require('../db/phone-queries');
 const zipCodeQueries = require('../db/zip-code-queries');
+const verifyService = require('../services/zip-code-verify-service');
 
 
 
@@ -16,39 +17,49 @@ const postRecord = async (req, res, next) => {
     let phone_number = req.body.phone;
     let zip_code = req.body.zip_code;
 
-    Promise.all([
-        emailQueries.postEmail(uid, email), 
-        phoneQueries.postPhone(uid, phone_number), 
-        zipCodeQueries.postZipCode(uid, zip_code)
-    ])
-    .then((result) => {
-        result = {uid: uid, email: email, phone_number: phone_number, zip_code: zip_code};
+    verifyService.verifyZipCode(zip_code)
+    .then((valid) => {
 
-        res.status(200).json({data: result});
-    })
-    // Delete inserted record if any insertion fails
-    .catch((error) => {
+        if (!valid) {
+            res.status(400).json({message: "Invalid zip code"});
+            return;
+        }
 
         Promise.all([
-            emailQueries.deleteEmailByUid(uid), 
-            phoneQueries.deletePhoneByUid(uid),
-            zipCodeQueries.deleteZipCodeByUid(uid)
+            emailQueries.postEmail(uid, email), 
+            phoneQueries.postPhone(uid, phone_number), 
+            zipCodeQueries.postZipCode(uid, zip_code, valid)
         ])
         .then((result) => {
-
-            error.statusCode = 400;
-            next(error);
-
+            result = {uid: uid, email: email, phone_number: phone_number, zip_code: zip_code};
+    
+            res.status(200).json({data: result});
         })
-        .catch((err) => {
-
-            error.message = "Failed to clean up database after failed record insertion";
-            error.statusCode = 500;
-            next(error);
-
+        // Delete inserted record if any insertion fails
+        .catch((error) => {
+    
+            Promise.all([
+                emailQueries.deleteEmailByUid(uid), 
+                phoneQueries.deletePhoneByUid(uid),
+                zipCodeQueries.deleteZipCodeByUid(uid)
+            ])
+            .then((result) => {
+    
+                error.statusCode = 400;
+                next(error);
+    
+            })
+            .catch((err) => {
+    
+                error.message = "Failed to clean up database after failed record insertion";
+                error.statusCode = 500;
+                next(error);
+    
+            })
+    
         })
 
-    })
+    });
 };
 
 exports.postRecord = postRecord;
